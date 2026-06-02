6c732fc81657a11e9db1b564df1380ccaa247363
kunish <kunish.butt@gmail.com>
2026-06-02 18:40:54 +0800

    fix(ui): scan ui library sources so select/dialog reka-var classes compile

--- packages/ui/src/components/ui/select/SelectContent.vue
+++ packages/ui/src/components/ui/select/SelectContent.vue
@@ -32,14 +32,14 @@ const forwarded = useForwardPropsEmits(() => {
         cn(
           'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 max-h-(--reka-select-content-available-height) origin-(--reka-select-content-transform-origin) overflow-x-hidden rounded-md border shadow-md',
           position === 'popper'
-            ? 'min-w-[var(--reka-select-trigger-width)] data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1'
+            ? 'min-w-(--reka-select-trigger-width) data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1'
             : 'min-w-[8rem]',
           props.class,
         )
       "
     >
       <SelectScrollUpButton />
-      <SelectViewport :class="cn('p-1', position === 'popper' && 'w-full min-w-[var(--reka-select-trigger-width)]')">
+      <SelectViewport :class="cn('p-1', position === 'popper' && 'w-full min-w-(--reka-select-trigger-width)')">
         <slot />
       </SelectViewport>
       <SelectScrollDownButton />
