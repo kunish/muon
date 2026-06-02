1f52254c99242c531faf504e20f0eb80b686a78d
kunish <kunish.butt@gmail.com>
2026-06-02 18:09:34 +0800

    fix(ui): scroll tall dialogs and stop select dropdown clamping to trigger height

--- packages/ui/src/components/ui/select/SelectContent.vue
+++ packages/ui/src/components/ui/select/SelectContent.vue
@@ -38,15 +38,7 @@ const forwarded = useForwardPropsEmits(() => {
       "
     >
       <SelectScrollUpButton />
-      <SelectViewport
-        :class="
-          cn(
-            'p-1',
-            position === 'popper' &&
-              'h-[var(--reka-select-trigger-height)] w-full min-w-[var(--reka-select-trigger-width)]',
-          )
-        "
-      >
+      <SelectViewport :class="cn('p-1', position === 'popper' && 'w-full min-w-[var(--reka-select-trigger-width)]')">
         <slot />
       </SelectViewport>
       <SelectScrollDownButton />
